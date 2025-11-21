f7a567acf411b8d65a7880d023f0d1e4b538e744
BermuGod28 <thejuanjo1128@gmail.com>
2025-11-20 22:42:05 -0500

    frontend en proceso

--- Provider/OrderConsumer/obj/Debug/net8.0/OrderConsumer.AssemblyInfo.cs
+++ Provider/OrderConsumer/obj/Debug/net8.0/OrderConsumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderConsumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cacd9146776d0bb7b1ea8620765fd69cb818c2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderConsumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderConsumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
