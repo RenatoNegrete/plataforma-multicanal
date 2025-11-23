f06595484d53f385b7924b935797fdc1789f904a
Renato Negrete <negreterenato@gmail.com>
2025-11-23 17:45:09 -0500

    Microservicio de usuarios

---
+++ Backend/Usuarios/src/main/java/org/example/demo3/Modelo/Mensaje.java
@@ -0,0 +1,17 @@
+package org.example.demo3.Modelo;
+
+public class Mensaje {
+
+    String mensaje;
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
