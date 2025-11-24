0520ab8eba02f70ab4fcf56ec71b541330737eea
Renato Negrete <negreterenato@gmail.com>
2025-11-23 20:04:44 -0500

    changes

--- Provider/ProviderData/obj/Debug/net8.0/ProviderData.AssemblyInfo.cs
+++ Provider/ProviderData/obj/Debug/net8.0/ProviderData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProviderData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ac1b350c361776f80b936558370eaed491f360")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProviderData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProviderData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
